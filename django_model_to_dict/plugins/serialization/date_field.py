b85da52b3b3e1313536a165e322825c92195ae93
zodman <zodman@gmail.com>
2018-02-09 17:10:34 -0600

    str to python3

--- django_model_to_dict/plugins/serialization/date_field.py
+++ django_model_to_dict/plugins/serialization/date_field.py
@@ -9,6 +9,9 @@ class DateFieldSerializationPlugin(SerializationPlugin):
     def serialize_field(field, model_instance):
         file_obj = field.value_from_object(model_instance)
         if file_obj:
+            if isinstance(file_obj, str): # PY3
+                return file_obj
+
             for format in settings.DATE_INPUT_FORMATS:
                 result = file_obj.strftime(format)
                 if result:
